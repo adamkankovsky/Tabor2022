5cc7586a5fe188e7cc0d1a6752d2dbb87deb5713
Adam Kankovsky <adamkankovsky@gmail.com>
2022-07-06 11:11:04 +0200

    qr code reader ready led blink

--- Tabor2022_Program/main.py
+++ Tabor2022_Program/main.py
@@ -1,69 +1,40 @@
 from __future__ import print_function
-from flask import Flask, Response
-from pyzbar import pyzbar
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from datetime import datetime
-
-import numpy as np
 import cv2
-import time
+import pyzbar.pyzbar as pyzbar
+from datetime import datetime
+from time import sleep
+import RPi.GPIO as GPIO
 
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-time.sleep(0.1)
+#width = 2592
+#height = 1944
 
-app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+#camera.set(3,width)
+#camera.set(4,height)
 
+def decodeCam(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    barcodes = pyzbar.decode(gray)
+    print('reading...', end='\r')
+    for barcode in barcodes:
+        barcodeData = barcode.data.decode()
+        barcodeType = barcode.type
+        print("["+str(datetime.now())+"] Type:{} | Data: {}".format(barcodeType, barcodeData))
+    return image
 
-@app.route('/stream')
-def stream():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-def gen():
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+try:
+    GPIO.output(11, GPIO.HIGH)  # Turn on
+    sleep(2)  # Sleep for 1 second
+    GPIO.output(11, GPIO.LOW)  # Turn off
+    sleep(2)
+    GPIO.output(11, GPIO.HIGH)  # Turn on
+    sleep(2)  # Sleep for 1 second
+    GPIO.output(11, GPIO.LOW)  # Turn off
     while True:
-        frame = get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-def get_frame():
-    camera.capture(rawCapture, format="bgr", use_video_port=True)
-    frame = rawCapture.array
-    process_frame(frame)
-    ret, jpeg = cv2.imencode('.jpg', frame)
-    rawCapture.truncate(0)
-
-    return jpeg.tobytes()
-
-
-def process_frame(frame):
-    decoded_objs = decode(frame)
-    draw_positions(frame, decoded_objs)
-
-
-def decode(frame):
-    decoded_objs = pyzbar.decode(frame, scan_locations=True)
-    for decoded_obj in decoded_objs:
-        print(datetime.now().strftime('%H:%M:%S.%f'))
-        print('Type: ', decoded_obj.type)
-        print('Data: ', decoded_obj.data)
-
-    return decoded_objs
-
-
-def draw_positions(frame, decoded_objs):
-    for decoded_obj in decoded_objs:
-        left, top, width, height = decoded_obj.rect
-        frame = cv2.rectangle(frame,
-                              (left, top),
-                              (left + width, height + top),
-                              (0, 255, 0), 2)
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=False, threaded=True)
+    # Read current frame
+    ret, frame = camera.read()
+    im=decodeCam(frame)
+except KeyboardInterrupt:
+ print('interrupted!')
